e875948f95c5b7692345cd3c9de7b4a1bbcc59d8
Paige Boyer <paige1381@gmail.com>
2018-01-23 15:39:55 -0500

    created RSVP list

--- public/app.js
+++ public/app.js
@@ -7,6 +7,8 @@ app.controller('MainController', ['$http', function($http) {
   this.arrAttending = [];
   this.arrNotAttending = [];
 
+  this.url = 'http://localhost:3000/'
+
   this.numAttendingRows = () => {
     for (let i = 0; i < this.numAttending; i++) {
       this.arrAttending.push(i);
@@ -18,5 +20,47 @@ app.controller('MainController', ['$http', function($http) {
       this.arrNotAttending.push(i);
     }
   }
+
+  // this.getRSVPList = () => {
+  //   $http({
+  //     method: 'GET',
+  //     url: this.url + 'rsvps'
+  //   }).then(response => {
+  //     this.rsvps = response.data;
+  //     console.log(this.rsvps);
+  //   }).catch(error => {
+  //     console.log('error:', error);
+  //   });
+  // }
+
+  // this.getRSVPList();
+
+  this.getAttendingList = () => {
+    $http({
+      method: 'GET',
+      url: this.url + 'guests/attending'
+    }).then(response => {
+      this.attendingRsvps = response.data;
+      this.attendingCount = this.attendingRsvps.length;
+    }).catch(error => {
+      console.log('error:', error);
+    });
+  }
+
+  this.getAttendingList();
+
+  this.getNotAttendingList = () => {
+    $http({
+      method: 'GET',
+      url: this.url + 'guests/notAttending'
+    }).then(response => {
+      this.notAttendingRsvps = response.data;
+      this.notAttendingCount = this.notAttendingRsvps.length;
+    }).catch(error => {
+      console.log('error:', error);
+    });
+  }
+
+  this.getNotAttendingList();
 
 }])
