e3004a7a5a34ba89ce9e7fde60ffbf9d5736627b
Paige Boyer <paige1381@gmail.com>
2018-01-22 20:22:30 -0500

    created front end rsvp form

--- public/app.js
+++ public/app.js
@@ -2,7 +2,21 @@ const app = angular.module('wedding-site-app', []);
 
 app.controller('MainController', ['$http', function($http) {
   this.rsvpModal = false;
-  this.numAttending = 0;
-  this.numNotAttending = 0;
+  this.numAttending = null;
+  this.numNotAttending = null;
+  this.arrAttending = [];
+  this.arrNotAttending = [];
+
+  this.numAttendingRows = () => {
+    for (let i = 0; i < this.numAttending; i++) {
+      this.arrAttending.push(i);
+    }
+  }
+
+  this.numNotAttendingRows = () => {
+    for (let i = 0; i < this.numNotAttending; i++) {
+      this.arrNotAttending.push(i);
+    }
+  }
 
 }])
