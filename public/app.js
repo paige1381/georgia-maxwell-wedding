16e0293e68eddebba4a6ae1710f106c1ffd8b947
Paige Boyer <paige1381@gmail.com>
2018-01-24 18:22:21 -0500

    completed edit rsvp

--- public/app.js
+++ public/app.js
@@ -1,7 +1,11 @@
 const app = angular.module('wedding-site-app', []);
 
 app.controller('MainController', ['$http', function($http) {
+  this.rsvpLanding = true;
+  this.rsvpForm = false;
+  this.rsvpUpdate = false;
   this.rsvpModal = false;
+  this.updateModal = false;
   this.rsvp = null;
   this.numAttending = null;
   this.numNotAttending = null;
@@ -10,10 +14,14 @@ app.controller('MainController', ['$http', function($http) {
   this.rsvpFormData = {};
   this.attendingFormData = [];
   this.notAttendingFormData = [];
+  this.editAttending = [];
+  this.editNotAttending = [];
 
   this.url = 'http://localhost:3000/'
 
   console.log('rsvp:', this.rsvp);
+  // console.log('editAttending:', this.editAttending);
+  // console.log('editNotAttending:', this.editNotAttending);
 
   this.numAttendingRows = () => {
     for (let i = 0; i < this.numAttending; i++) {
@@ -56,7 +64,6 @@ app.controller('MainController', ['$http', function($http) {
   this.getNotAttendingList();
 
   this.processRSVPSignIn = () => {
-    console.log('rsvp:', this.rsvp);
     $http({
       method: 'POST',
       url: this.url + 'rsvps',
@@ -65,6 +72,8 @@ app.controller('MainController', ['$http', function($http) {
       this.rsvp = response.data
       this.rsvpModal = false;
       this.rsvpFormData = {};
+      this.rsvpLanding = false;
+      this.rsvpForm = true;
       console.log('rsvp:', this.rsvp);
     }).catch(error => {
       console.log('error:', error);
@@ -107,13 +116,73 @@ app.controller('MainController', ['$http', function($http) {
       console.log('Record added:', response.data);
       this.getAttendingList();
       this.getNotAttendingList();
+      this.rsvpForm = false;
+      this.rsvpLanding = true;
     }).catch(error => {
       console.log('error:', error);
     })
   }
 
+  this.getRSVPByEmail = (email) => {
+    console.log('editAttending:', this.editAttending);
+    console.log('editNotAttending:', this.editNotAttending);
+    console.log('edit email:', email);
+    for (let i = 0; i < this.attendingRsvps.length; i++) {
+      if (this.attendingRsvps[i].rsvp.email.toLowerCase() === email.toLowerCase()) {
+        this.editAttending.push(this.attendingRsvps[i])
+      }
+    }
+    for (let i = 0; i < this.notAttendingRsvps.length; i++) {
+      if (this.notAttendingRsvps[i].rsvp.email.toLowerCase() === email.toLowerCase()) {
+        this.editNotAttending.push(this.notAttendingRsvps[i])
+      }
+    }
+    console.log('records before edit:', this.editAttending);
+    console.log('records before edit:', this.editNotAttending);
+    this.updateModal = false;
+    this.rsvpLanding = false;
+    this.rsvpUpdate = true;
+    this.numAttending = this.editAttending.length;
+    this.numNotAttending = this.editNotAttending.length;
+    this.numAttendingRows()
+    this.numNotAttendingRows()
+    this.rsvpFormData = {};
+  }
+
+  this.formatRSVPUpdateData = () => {
+    console.log('records after edit:', this.editAttending);
+    console.log('records after edit:', this.editNotAttending);
+    for (var i = 0; i < this.editAttending.length; i++) {
+      this.updateRSVP(this.editAttending[i])
+    }
+    for (var i = 0; i < this.editNotAttending.length; i++) {
+      this.updateRSVP(this.editNotAttending[i])
+    }
+  }
 
 
+  this.updateRSVP = (rsvp) => {
+    console.log(rsvp);
+    $http({
+      method: 'PUT',
+      url: this.url + 'guests/' + rsvp.id,
+      data: rsvp
+    }).then(response => {
+      console.log('Record updated:', response.data);
+      this.getAttendingList();
+      this.getNotAttendingList();
+      this.rsvpUpdate = false;
+      this.rsvpLanding = true;
+      this.editAttending = [];
+      this.editNotAttending = [];
+      this.numAttending = null;
+      this.numNotAttending = null;
+      this.arrAttending = [];
+      this.arrNotAttending = [];
+    }).catch(error => {
+      console.log('error:', error);
+    })
+  }
 
 
 
